a1dc40b3b4e60274dbabdf6124f26b18eedfce05
Pichler <gerhard.pichler@bearingpoint.com>
2019-10-30 17:44:44 +0100

    test54

--- push.bat
+++ push.bat
@@ -2,9 +2,10 @@ rem echo "# PhoneGapPlayGround" >> README.md
 rem git init
 git add .
 
-SET /P _inputname= Enter the commit message::
-
+echo OFF
+SET /P _inputname= Enter the commit message:
 git commit -m "%_inputname%"
+echo ON
 
 rem git remote add origin https://github.com/kwon66/PhoneGapPlayGround.git
 git push -u origin master
