f985256a14019074163b408097f0ec6f68d3fa80
Pichler <gerhard.pichler@bearingpoint.com>
2019-10-30 17:35:51 +0100

    2

---
+++ push.bat
@@ -0,0 +1,6 @@
+# echo "# PhoneGapPlayGround" >> README.md
+# git init
+git *
+git commit -m "first commit"
+# git remote add origin https://github.com/kwon66/PhoneGapPlayGround.git
+git push -u origin master
